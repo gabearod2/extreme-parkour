8e80ce81f918e1ebe3aa990b9893ca13907cdda5
gabearod2 <gabearod2@gmail.com>
2024-08-04 11:37:01 -0400

    notes

--- legged_gym/legged_gym/envs/base/legged_robot.py
+++ legged_gym/legged_gym/envs/base/legged_robot.py
@@ -134,8 +134,9 @@ class LeggedRobot(BaseTask):
         self.total_env_steps_counter += 1
         clip_actions = self.cfg.normalization.clip_actions / self.cfg.control.action_scale
         self.actions = torch.clip(actions, -clip_actions, clip_actions).to(self.device)
+        
+        # step physics and render each frame
         self.render()
-
         for _ in range(self.cfg.control.decimation):
             self.torques = self._compute_torques(self.actions).view(self.torques.shape)
             self.gym.set_dof_actuation_force_tensor(self.sim, gymtorch.unwrap_tensor(self.torques))
@@ -144,6 +145,7 @@ class LeggedRobot(BaseTask):
             self.gym.refresh_dof_state_tensor(self.sim)
         self.post_physics_step()
 
+        #return clipped obs, clipped states (None), rewards, dones, and infos
         clip_obs = self.cfg.normalization.clip_observations
         self.obs_buf = torch.clip(self.obs_buf, -clip_obs, clip_obs)
         if self.privileged_obs_buf is not None:
